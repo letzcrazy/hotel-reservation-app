ff824f675f1ab300458fb3f4d40da8c548cfce5f
letzcrazy <okamotoryunosuke1108@gmail.com>
2023-09-14 08:25:35 -0500

    Added 'add reservation' button to list view

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Reservation } from '../models/reservation';
 import { ReservationService } from '../reservation/reservation.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-list',
@@ -10,11 +11,22 @@ import { ReservationService } from '../reservation/reservation.service';
 export class ReservationListComponent implements OnInit {
   reservations: Reservation[] = [];
 
-  constructor(private reservationService: ReservationService) { }
+  constructor(
+    private reservationService: ReservationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.reservations = this.reservationService.getAllReservations();
     console.log(this.reservations);
+  }
+
+  deleteReservation(id: string) {
+    this.reservationService.deleteReservation(id);
+  }
+
+  addReservation() {
+    this.router.navigate(['/new']);
   }
 
 
