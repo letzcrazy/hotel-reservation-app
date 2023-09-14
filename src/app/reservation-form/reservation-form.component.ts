4c2f15d9cc012aa1278a209a4612c47bb5e1bd91
letzcrazy <okamotoryunosuke1108@gmail.com>
2023-09-14 07:29:43 -0500

    Added reservation form html

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -9,4 +9,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReservationFormComponent {
 
   reservationForm: FormGroup = new FormGroup({});
+
+  onSubmit() { }
 }
