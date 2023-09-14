f794d35acdd79022a14f736005528ef79450c365
letzcrazy <okamotoryunosuke1108@gmail.com>
2023-09-14 07:25:46 -0500

    Added form attributes and validators

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     ReservationFormComponent,
-    ReservationListComponent
+    ReservationListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReservationModule { }
